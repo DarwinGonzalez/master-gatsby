9213e251937793915bb686e5031590cb85d6b968
DarwinGonzalez <alu0100812794@ull.edu.es>
2020-10-01 13:21:21 +0100

    Adding some other components and setting up layout with gatsby

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Layout from '../components/Layout';
+import Nav from '../components/Nav';
 
 export default function HomePage() {
   return (
-    <div>
+    <>
       <p>Hey! I'm the homepage!</p>
-    </div>
+    </>
   );
 }
