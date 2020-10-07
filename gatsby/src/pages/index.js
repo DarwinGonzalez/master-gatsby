f5e3e5a66d99f320df606d65b98189a0255c96da
DarwinGonzalez <alu0100812794@ull.edu.es>
2020-10-07 10:53:45 +0100

    Adding global styles and custom typography

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import Nav from '../components/Nav';
 
 export default function HomePage() {
   return (
     <>
-      <p>Hey! I'm the homepage!</p>
+      <p> I'm the homepage mate! </p>
     </>
   );
 }
